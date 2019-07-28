e1050fae88aa9e21005a28b7a2901f6992989f5a
Mr. Bhat <spbhat672@gmail.com>
2019-07-29 04:19:13 +0530

    DD

---
+++ DeliveryDay/src/deliveryday/DeliveryDay.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deliveryday;
+
+/**
+ *
+ * @author Santosh
+ */
+public class DeliveryDay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DDForm form= new DDForm();
+        form.show();
+    }
+    
+}
